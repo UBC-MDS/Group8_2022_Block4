b366d316d87ec55f9da7b74d332f24f5681c73c8
MAHMOODUR RAHMAN <mahmoodr@student.ubc.ca>
2022-01-15 16:11:11 -0800

    Revised plot func docstrings

--- src/tweetlytics/tweetlytics.py
+++ src/tweetlytics/tweetlytics.py
@@ -94,22 +94,38 @@ def analytics(df,keyword):
     >>> report = analytics(df,keyword)
     """
 
-def plot_histo(df, time_def):
+def plot_tweets(df, time_def):
     """
     Plotting the number of tweets per hour, throughout 24 hours
+    Plotting hashtags in tweets, and plot the hashtag analysis.
+    Plotting user's tweets with sentimental analysis
+
     Parameters:
     -----------
     df : dataframe
         pandas dataframe
     time_def: string
         The column name of post time in dataframe.
+    tweet: string
+        The column name of tweet text in dataframe.
+    sentiment_df : dataframe
+        Output of tweet_sentiment_analysis,
+        dataframe that contains added columns from tweet_sentiment_analysis
+    plot_type : string
+        Optional: Type of plot to return, 3 options:'Standard', 'Stacked', and 'Separate'
+        'Standard' Returns bar plot of most common words tweeted color coded by sentiment
+        'Stacked' Returns same as 'Standard' but if words are found in other sentiments they are stacked together
+        'Separate' Returns 3 bar plots with the sentiment of 'Postive' 'Neutral', and 'Negative' separated
     Returns:
     --------
-    histo_plot: chart
+    plot: chart
         A histogram line plot plotting the counts of tweets versus hours.
+        A chart plotting analysis result of most frequent used hashtag words.
+        A bar plot of the user's tweets containing in order
+        the most common words, colour coded by the word's sentiment class.
         
     Examples
     --------
-    >>> from tweetlytics.tweetlytics import plot_histo
-    >>> histo_plot = plot_histo(df,time_def)
+    >>> from tweetlytics.tweetlytics import plot_tweets
+    >>> plot = plot_tweets(df,time_def)
     """
