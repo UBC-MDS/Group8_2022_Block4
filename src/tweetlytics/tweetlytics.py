cc6f0ec98cc55ae0aa4855a7c8fa8d3d5956908f
MAHMOODUR RAHMAN <mahmoodr@student.ubc.ca>
2022-01-21 21:52:22 -0800

    Added hashtag frequaency function

--- src/tweetlytics/tweetlytics.py
+++ src/tweetlytics/tweetlytics.py
@@ -9,6 +9,16 @@ import json
 import pandas as pd
 from dotenv import load_dotenv
 
+import altair as alt
+from textblob import TextBlob
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+nltk.download('stopwords')
+from nltk.stem import SnowballStemmer
+import string
+
 load_dotenv()  # load .env files in the project folder
 
 
@@ -179,38 +189,54 @@ def analytics(df, keyword):
     """
 
 
-def plot_tweets(df, time_def):
+def plot_freq(df, col_text):
     """
-    Plotting the number of tweets per hour, throughout 24 hours
-    Plotting hashtags in tweets, and plot the hashtag analysis.
-    Plotting user's tweets with sentimental analysis
+    Takes in dataframe and analyzes 
+        the hashtags to plot the most frequent ones.
 
     Parameters:
     -----------
-    df : dataframe
-        pandas dataframe
-    time_def: string
-        The column name of post time in dataframe.
-    tweet: string
+    df : A pandas dataframe 
+        consisting of the tweets
+
+    text: string
         The column name of tweet text in dataframe.
-    sentiment_df : dataframe
-        Output of tweet_sentiment_analysis,
-        dataframe that contains added columns from tweet_sentiment_analysis
-    plot_type : string
-        Optional: Type of plot to return, 3 options:'Standard', 'Stacked', and 'Separate'
-        'Standard' Returns bar plot of most common words tweeted color coded by sentiment
-        'Stacked' Returns same as 'Standard' but if words are found in other sentiments they are stacked together
-        'Separate' Returns 3 bar plots with the sentiment of 'Postive' 'Neutral', and 'Negative' separated
+
     Returns:
     --------
-    plot: chart
-        A histogram line plot plotting the counts of tweets versus hours.
+    hash_plot: A bar chart
         A chart plotting analysis result of most frequent used hashtag words.
-        A bar plot of the user's tweets containing in order
-        the most common words, colour coded by the word's sentiment class.
-
-    Examples
-    --------
-    >>> from tweetlytics.tweetlytics import plot_tweets
-    >>> plot = plot_tweets(df,time_def)
     """
+    # Checking for valid inputs
+    if not isinstance(df, pd.DataFrame):
+        raise Exception("The argunment, df should take in a pandas dataframe")
+    if type(col_text) != str:
+        raise Exception("The argunment, col_text should take in a string")
+
+    # get hashtags from text
+    df['hash'] = df[col_text].apply(lambda x: re.findall(r'[#]\w+', x))
+
+    # counting tags
+    hash_dict = {}
+    for hashtags in df["hash"]:
+        for word in hashtags:
+            hash_dict[word] = hash_dict.get(word, 0) + 1
+
+    hash_df = pd.DataFrame(columns=['Keyword', 'Count'])
+    for key, value in hash_dict.items():
+        key_value = [[key, value]]
+        hash_df = hash_df.append(pd.DataFrame(key_value, columns=['Keyword', 'Count']),
+                                       ignore_index=True)
+
+    # frequency plot
+    hash_plot = alt.Chart(hash_df).mark_bar().encode(
+        x=alt.X('Count', title="Hashtags"),
+        y=alt.Y('Keyword', title="Count of Hashtags", sort='-x')
+    ).properties(
+        title='Top 15 Hashtag Words'
+    ).transform_window(
+        rank='rank(Count)',
+        sort=[alt.SortField('Count', order='descending')]
+    ).transform_filter((alt.datum.rank <= 15))
+
+    return hash_plot
